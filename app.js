ae74c624fcebc51fb15b8ec44f4717cbfcdbdab6
Eddie Ruano <eddieruano@gmail.com>
2017-06-06 18:54:50 -0700

    Script

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var job;
 var msgQueueIDs = [];
 var numberOfQueuedMsgs = 0;
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Message Variables
 var message;
