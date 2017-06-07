06f4d08c5b776aa9ec01870ecd7054cade97a2ae
Eddie Ruano <eddieruano@gmail.com>
2017-06-07 01:01:34 -0700

    Script

--- messageQueue.js
+++ messageQueue.js
@@ -8,13 +8,16 @@
  * @param {[type]} obj [object to enqueue]
  */
 
-var MessPackage = require('./MessagePackage.js');
-
-function MessageQueue() {
+function MessageQueue(obj) {
 	this.messageQueueSize = 0;
 	this.messageQueueArray = [];
-}
 
+	this.queuePrint = function(obj) {
+		console.log("Size: " + obj.messageQueueSize);
+		console.log("Contents: " + obj.messageQueueArray);
+	};
+
+}
 // // prints MessageQueue information
 // MessageQueue.prototype.queuePrint = function(queue) {
 // 	console.log("Size: " + queue.messageQueueSize);
